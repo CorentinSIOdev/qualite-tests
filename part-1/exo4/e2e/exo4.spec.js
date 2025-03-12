a3480135495b775f59b24ae984f5a0a73ff86108
CorentinSIOdev <bonifaciocorentin@gmail.com>
2025-03-12 18:59:26 +0100

    Nouvelle version de la partie 1

--- part-1/exo4/e2e/exo4.spec.js
+++ part-1/exo4/e2e/exo4.spec.js
@@ -1,18 +1,21 @@
 const { test, expect } = require('@playwright/test');
 
 test('Formulaire de connexion fonctionne correctement', async ({ page }) => {
-  await page.goto('http://localhost:3000/login.html');
+  await page.goto('https://the-internet.herokuapp.com/login');
 
-  const emailInput = page.locator('input[type="email"]');
-  const passwordInput = page.locator('input[type="password"]');
+  const usernameInput = page.locator('#username');
+  const passwordInput = page.locator('#password');
   const submitBtn = page.locator('button[type="submit"]');
 
-  await expect(emailInput).toBeVisible();
+  await expect(usernameInput).toBeVisible();
   await expect(passwordInput).toBeVisible();
 
-  await emailInput.fill('test@example.com');
-  await passwordInput.fill('supersecret');
+  await usernameInput.fill('tomsmith');
+  await passwordInput.fill('SuperSecretPassword!');
   await submitBtn.click();
 
-  await expect(page).toHaveURL(/dashboard\.html/);
+  await expect(page).toHaveURL('https://the-internet.herokuapp.com/secure');
+
+  const successMsg = page.locator('.flash.success');
+  await expect(successMsg).toContainText('You logged into a secure area!');
 });
